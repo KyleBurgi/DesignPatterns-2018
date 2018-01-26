3efda41c32b8a5557dc55577bf5f69b009478472
kyle burgi <kyleburgi@gmail.com>
2018-01-26 12:50:52 -0800

    Created initial file for Decorator Pattern

---
+++ burgikyle_cscd349as3/OhChristmasTree/OhChristmasTree/OhChristmasTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OhChristmasTree
+{
+    class OhChristmasTree
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Oh Christmas Tree, Oh Christmas Tree...");
+            String temp = ReadLine();
+        }
+    }
+}
